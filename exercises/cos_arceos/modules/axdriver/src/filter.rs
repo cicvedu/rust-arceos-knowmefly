e66970aa0df11477d2a53817c1040064e2a5bc88
knowmefly <knowmefly@qq.com>
2023-08-27 15:19:51 +0800

    do test a4

--- exercises/cos_arceos/modules/axdriver/src/filter.rs
+++ exercises/cos_arceos/modules/axdriver/src/filter.rs
@@ -1,3 +1,65 @@
+use super::prelude::*;
+use driver_common::BaseDriverOps;
+use crate::virtio::VirtIoHalImpl;
+type VirtIoTransport = driver_virtio::MmioTransport;
+
 pub struct NetFilter<T> {
     pub inner: T,
+}
+
+impl BaseDriverOps for NetFilter<driver_virtio::VirtIoNetDev<VirtIoHalImpl, VirtIoTransport, 64>> {
+    fn device_type(&self) -> driver_common::DeviceType {
+        driver_common::DeviceType::Net
+    }
+
+    fn device_name(&self) -> &str {
+        "my-net"
+    }
+}
+
+impl NetDriverOps for NetFilter<driver_virtio::VirtIoNetDev<VirtIoHalImpl, VirtIoTransport, 64>> {
+    #[inline]
+
+    fn mac_address(&self) -> driver_net::EthernetAddress {
+        self.inner.mac_address()
+    }
+
+    fn can_transmit(&self) -> bool {
+        self.inner.can_transmit()
+    }
+
+    fn can_receive(&self) -> bool {
+        self.inner.can_receive()
+    }
+
+    fn rx_queue_size(&self) -> usize {
+        self.inner.rx_queue_size()
+    }
+
+    fn tx_queue_size(&self) -> usize {
+        self.inner.tx_queue_size()
+    }
+
+    fn recycle_rx_buffer(&mut self, rx_buf: driver_net::NetBufPtr) -> driver_common::DevResult {
+        self.inner.recycle_rx_buffer(rx_buf)
+    }
+
+    fn recycle_tx_buffers(&mut self) -> driver_common::DevResult {
+        self.inner.recycle_tx_buffers()
+    }
+
+    fn alloc_tx_buffer(&mut self, size: usize) -> driver_common::DevResult<driver_net::NetBufPtr> {
+        self.inner.alloc_tx_buffer(size)
+    }
+
+    fn transmit(&mut self, tx_buf: driver_net::NetBufPtr) -> driver_common::DevResult {
+        log::warn!("Filter: transmit len [{}]", tx_buf.packet_len());
+        self.inner.transmit(tx_buf)
+    }
+
+    fn receive(&mut self) -> driver_common::DevResult<driver_net::NetBufPtr> {
+        let ret = self.inner.receive()?;
+        log::warn!("Filter: receive len [{:?}]", ret.packet_len());
+        Ok(ret)
+    }
 }
